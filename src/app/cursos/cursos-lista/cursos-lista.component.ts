73c49f2bd4848964dce4c2ed38bdf83ad309ada8
Gabriel Duarte <gabrield.developer@gmail.com>
2021-07-12 22:02:27 -0300

    feat: implements reactive error handling to a subscription

--- src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { empty, Observable, Subject } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { Curso } from '../curso';
 import { CursosService } from '../cursos.service';
 
@@ -11,6 +12,7 @@ import { CursosService } from '../cursos.service';
 export class CursosListaComponent implements OnInit {
 
   cursos$: Observable<Curso[]>;
+  error$ = new Subject<boolean>();
 
   constructor(
     private service: CursosService
@@ -18,7 +20,14 @@ export class CursosListaComponent implements OnInit {
 
   ngOnInit(): void {
     // this.service.list().subscribe(res => this.cursos = res);
-    this.cursos$ = this.service.list();
+    this.cursos$ = this.service.list()
+    .pipe(
+      catchError(err => {
+        console.error(err);
+        this.error$.next(true);
+        return empty();
+      })
+    );
   }
 
 }

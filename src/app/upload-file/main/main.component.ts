3ffd1c6da29afda459aff6beccbfc54f41a5ddd2
Gabriel Duarte <gabrield.developer@gmail.com>
2021-07-18 23:32:00 -0300

    feat: request a formdata while uploading a new file to server

--- src/app/upload-file/main/main.component.ts
+++ src/app/upload-file/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UploadFileService } from '../upload-file.service';
 
 @Component({
   selector: 'app-main',
@@ -7,14 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  public files: Set<File>;
+  private subscriptions = [];
+
+  constructor(private service: UploadFileService) { }
 
   ngOnInit(): void {
   }
 
   onChange(event) {
     const selectedFiles = <FileList>event.srcElement.files;
+    this.files = new Set();
+    for(let i = 0; i < selectedFiles.length; i++) {
+      this.files.add(selectedFiles[i]);
+    }
+
     console.log(selectedFiles.item)
+  }
+
+  onUpload() {
+    if(this.files && this.files.size > 0) {
+      const sub = this.service.upload(this.files, 'http://localhost:8000/upload').subscribe(
+        res => console.log('upload concluido', res)
+      )
+      this.subscriptions.push(sub) 
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
   }
 
 }

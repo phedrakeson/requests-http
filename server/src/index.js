87a1b6aed6e28ce246429078847f35c91cd5668b
Gabriel Duarte <gabrield.developer@gmail.com>
2021-07-18 23:03:32 -0300

    feat: implements a simple backend service for file uploading

---
+++ server/src/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const multiparty = require('connect-multiparty');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
+
+const multipartyMiddleware = multiparty({ uploadDir: './uploads' });
+app.post('/upload', multipartyMiddleware, (req, res) => {
+  const files = req.files;
+  console.log(files);
+  res.json({
+    message: files
+  });
+});
+
+app.use((err,req, res, next) => res.json({error: err.messsage}));
+
+app.listen(8000, () => {
+  console.log('Servidor inicializado! (porta-8000)')
+});
+

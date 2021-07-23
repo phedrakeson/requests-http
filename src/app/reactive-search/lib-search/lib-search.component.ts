3bfcbaad049be91aeea7664b99ec8b517a783bb7
Gabriel Duarte <gabrield.developer@gmail.com>
2021-07-22 23:39:49 -0300

    refactor: implements a better way to pass on httpParams to a request

--- src/app/reactive-search/lib-search/lib-search.component.ts
+++ src/app/reactive-search/lib-search/lib-search.component.ts
@@ -1,5 +1,8 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-lib-search',
@@ -9,14 +12,35 @@ import { FormControl } from '@angular/forms';
 export class LibSearchComponent implements OnInit {
 
   queryField = new FormControl();
+  readonly SEARCH_URL = 'https://api.cdnjs.com/libraries';
+  results$: Observable<any>;
+  total: number;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
   ngOnInit(): void {
   }
 
   onSearch() {
-    console.log(this.queryField.value)
+    let value = this.queryField.value;
+    const fields = 'name,description,version,homepage';
+    const params = {
+      search: value,
+      fields: fields
+    };
+
+    let params2: HttpParams = new HttpParams();
+    params2 = params2.set('search', value);
+    params2 = params2.set('fields', fields);
+
+    if(value && (value = value.trim() ) !== '') {
+      this.results$ = this.http.get(this.SEARCH_URL, { params: params2 }).pipe(
+        tap( (res: any) => this.total = res.total),
+        map( (res: any) => res.results)
+      );
+    }
   }
 
 }

f8131a9f572a92ffbdc87e91d9b3dead10328bfb
Gabriel Duarte <gabrield.developer@gmail.com>
2021-07-22 23:53:21 -0300

    feat: implements reactive searching through text input

--- src/app/reactive-search/lib-search/lib-search.component.ts
+++ src/app/reactive-search/lib-search/lib-search.component.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, filter, map, switchMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-lib-search',
@@ -15,32 +15,32 @@ export class LibSearchComponent implements OnInit {
   readonly SEARCH_URL = 'https://api.cdnjs.com/libraries';
   results$: Observable<any>;
   total: number;
+  readonly FIELDS = 'name,description,version,homepage';
+
 
   constructor(
     private http: HttpClient
   ) { }
 
   ngOnInit(): void {
+    // busca reativa
+    this.results$ = this.queryField.valueChanges.pipe(
+      map(value => value.trim()),
+      filter(value => value.length > 1),
+      debounceTime(200),
+      distinctUntilChanged(),
+      switchMap(value => this.http.get(this.SEARCH_URL, {
+        params: {
+          search: value,
+          fields: this.FIELDS
+        }
+      })),
+      tap( (res: any) => this.total = res.total),
+      map( (res: any) => res.results)
+    );
   }
 
   onSearch() {
-    let value = this.queryField.value;
-    const fields = 'name,description,version,homepage';
-    const params = {
-      search: value,
-      fields: fields
-    };
-
-    let params2: HttpParams = new HttpParams();
-    params2 = params2.set('search', value);
-    params2 = params2.set('fields', fields);
-
-    if(value && (value = value.trim() ) !== '') {
-      this.results$ = this.http.get(this.SEARCH_URL, { params: params2 }).pipe(
-        tap( (res: any) => this.total = res.total),
-        map( (res: any) => res.results)
-      );
-    }
   }
 
 }
